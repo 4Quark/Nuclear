fff17d52722534dd99c53b3849bea3fcbc812462
4Quark <mariya_baranova@list.ru>
2024-08-08 21:11:27 +0300

    init: start nuclear

---
+++ Nuclear/src/App.tsx
@@ -0,0 +1,15 @@
+import nuclearLogo from './assets/brew.webp'
+
+function App() {
+  return (
+    <>
+        <a href="https://react.dev" target="_blank">
+          <img src={nuclearLogo} className="logo react" alt="React logo" />
+        </a>
+      <h1>Nuclear</h1>
+      
+    </>
+  )
+}
+
+export default App
